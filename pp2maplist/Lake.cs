cbe06717d840777258100da32bbea8809cff5f93
Topias Mariapori <topias@mariapori.fi>
2020-12-12 22:42:56 +0200

    Add project files.

---
+++ pp2maplist/Lake.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pp2maplist
+{
+    public class Lake
+    {
+        public string lakeName { get; set; }
+        public string gameTime { get; set; }
+        public string gameMode { get; set; }
+    }
+}
